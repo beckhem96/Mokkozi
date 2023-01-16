d4398ddb796043df47953b61d442b25c027918da
beckhem96 <beckhem96@naver.com>
2023-01-16 11:19:00 +0900

    ✨ feat : react-router-dome 설치 후 Home, MyStudy로 이동 가능한 Navbar생성

--- FE/mokkozi/src/App.tsx
+++ FE/mokkozi/src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import MyStudy from './pages/MyStudy';
+
+// 아래는 뭐 어따 쓰는건지?
+// declare function BrowserRouter(props: BrowserRouterProps): React.ReactElement;
+// interface BrowserRouterProps {
+//   basename?: string;
+//   children?: React.ReactNode;
+//   window?: Window;
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="mystudy/:userId" element={<MyStudy />}></Route>
+          {/* userId로 해당 사용자만 접급할 수 있는거 찾아보도록 */}
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
