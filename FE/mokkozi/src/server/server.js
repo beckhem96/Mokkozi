0267de67fef9b53009ae501187240a4a9a3ba40c
beckhem96 <beckhem96@naver.com>
2023-02-26 02:10:03 +0900

    react에 express 서버 설치 후 socket.io room 적용

---
+++ FE/mokkozi/src/server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http');
+const SocketIO = require('socket.io');
+const handleListen = () => console.log(`Listning on http://localhost:8080`);
+
+app.use(express.static(path.join(__dirname, '../../public')));
+
+app.get('/', function (res, req) {
+  req.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000',
+    allowedHeaders: ['my-custom-header'],
+    credentials: true,
+  },
+});
+
+wsServer.on('connection', (socket) => {
+  socket.onAny((event) => {
+    console.log(`Socket Envet: ${event}`);
+  });
+  socket.on('join_room', (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit('welcome');
+  });
+});
+httpServer.listen(8080, handleListen);
