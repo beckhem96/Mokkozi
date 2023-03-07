e0b5591db61ea0fbc1a427ba3df2be0313a107d1
beckhem96 <beckhem96@naver.com>
2023-03-07 21:26:07 +0900

    :sparkles: feature: offer, answert, ice 완료: peer 카메라 연결됨

--- FE/mokkozi/src/components/FaceCall.tsx
+++ FE/mokkozi/src/components/FaceCall.tsx
@@ -1,5 +1,5 @@
 'use strict';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { createElement, useEffect, useRef, useState } from 'react';
 import { io } from 'socket.io-client';
 import Button from '../styles/Button';
 
@@ -7,7 +7,7 @@ import Button from '../styles/Button';
 import '../styles/Modal.css';
 function FaceCall() {
   const localVideo: any = useRef(HTMLMediaElement);
-  const peer1Video: any = useRef();
+  const peerVideo: any = useRef(HTMLMediaElement);
   const [chats, setChats] = useState<Array<{ key: number; value: string }>>([]);
   const [roomName, setRoomName] = useState('');
   let myDataChannel;
@@ -26,13 +26,20 @@ function FaceCall() {
   useEffect(() => {
     socket.on('welcome', async () => {
       myDataChannel = myPeerConnection.createDataChannel('chat');
-      myDataChannel.addEventListener('message', console.log);
+      myDataChannel.onmessage = (event: any) => {
+        console.log(event.data);
+        console.log('ss');
+      };
       console.log('made data channel');
       const offer = await myPeerConnection.createOffer();
       myPeerConnection.setLocalDescription(offer);
+      sendEmit('offer', offer, roomName);
       console.log('sent offer');
-      socket.emit('offer', offer, roomName);
       addChat('어서와');
+    });
+    socket.on('answer', (answer) => {
+      console.log('received answer');
+      myPeerConnection.setRemoteDescription(answer);
     });
     socket.on('offer', async (offer) => {
       myPeerConnection.addEventListener('datachannel', (event: any) => {
@@ -43,23 +50,47 @@ function FaceCall() {
       const answer = await myPeerConnection.createAnswer();
       myPeerConnection.setLocalDescription(answer);
       socket.emit('answer', answer, roomName);
-      console.log('sent answer');
     });
-    // return () => {
-    //   socket.disconnect();
-    // };
+    socket.on('ice', (ice) => {
+      myPeerConnection.addIceCandidate(ice);
+    });
+    return () => {
+      socket.off('welcome');
+      socket.off('offer');
+      socket.off('answer');
+      socket.off('ice');
+    };
   }, []);
 
-  async function initCall(event: React.ChangeEvent) {
-    socket.emit('join_room', 'myStudy');
+  async function initConnection() {
+    setRoomName('myStudy');
+    await initCall();
+    socket.emit('join_room', roomName);
+  }
+
+  function sendEmit(message: string, data: any, roomName: string) {
+    socket.emit(message, data, roomName);
+  }
+
+  async function initCall() {
     await getMedia(null);
     makeConnection();
-    setRoomName('test');
   }
   /** RTC 연결하는 함수 */
   function makeConnection() {
-    myPeerConnection = new RTCPeerConnection();
-    console.log(myPeerConnection);
+    myPeerConnection = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: [
+            'stun:stun.l.google.com:19302',
+            'stun:stun1.l.google.com:19302',
+            'stun:stun2.l.google.com:19302',
+            'stun:stun3.l.google.com:19302',
+            'stun:stun4.l.google.com:19302',
+          ],
+        },
+      ],
+    });
     myPeerConnection.addEventListener('icecandidate', handleIce);
     myPeerConnection.addEventListener('addstream', handleAddStream);
     myStream
@@ -67,13 +98,11 @@ function FaceCall() {
       .forEach((track: any) => myPeerConnection.addTrack(track, myStream));
   }
   function handleIce(data: { candidate: any }) {
-    socket.emit('ice', data.candidate, roomName);
+    sendEmit('ice', data.candidate, roomName);
   }
 
   function handleAddStream(data: any) {
-    console.log(data);
-    // const peerFace = document.getElementById('peerFace');
-    // peerFace.srcObject = data.stream;
+    peerVideo.current.srcObject = data.stream;
   }
 
   /** 유저의 미디어 정보를 불러오는 함수 */
@@ -109,8 +138,17 @@ function FaceCall() {
         width={'400'}
         height={'400'}
       ></video>
+      <video
+        ref={peerVideo}
+        id="peer"
+        autoPlay
+        playsInline
+        muted
+        width={'400'}
+        height={'400'}
+      ></video>
       {/* <video ref={peer1Video} id="peer1" autoPlay playsInline muted></video> */}
-      <Button onClick={initCall}>연결</Button>
+      <Button onClick={initConnection}>연결</Button>
 
       <div>
         <h1>Chat</h1>
